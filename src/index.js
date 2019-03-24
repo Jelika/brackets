136e38b21d830acc06441e0edb3f98a0a4160294
Jelika <47065104+Jelika@users.noreply.github.com>
2019-03-25 00:01:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	const stack = [];
+	for (let i = 0; i < str.length; i++) { //check all elements
+		let bool = false;
+		for (let j = 0; j < bracketsConfig.length; j++) {
+			if (str[i] === bracketsConfig[j][0]) {
+				bool = true;
+					};
+				}
+					if (bool && stack[stack.length -1] !== str[i]) {
+				stack.push(str[i]);
+			}
+		
+
+	 else {
+			let configIndex;
+			for (let j = 0; j < bracketsConfig.length; j++) {
+				if (stack[stack.length - 1] === bracketsConfig[j][0]) {
+					configIndex = j;
+				}
+			}
+			if (bracketsConfig[configIndex]) {
+				if (bracketsConfig[configIndex][1] === str[i]) {
+					stack.pop();
+				} else {
+					stack.push(str[i]);
+				}
+			}
+
+		}
+	}
+
+	return !stack.length
 }
+
